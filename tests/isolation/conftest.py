b7b33c94455cb38abddee9810ca9626709efa0f2
John Pollock <pollockjj@gmail.com>
2026-05-27 05:03:59 -0500

    Fix isolation test harness

---
+++ tests/isolation/conftest.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+import asyncio
+import inspect
+
+
+def pytest_pyfunc_call(pyfuncitem):
+    test_func = pyfuncitem.obj
+    if not inspect.iscoroutinefunction(test_func):
+        return None
+    kwargs = {
+        name: pyfuncitem.funcargs[name]
+        for name in pyfuncitem._fixtureinfo.argnames
+    }
+    asyncio.run(test_func(**kwargs))
+    return True
